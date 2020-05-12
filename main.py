6285e43a0fdb102ac56af24a5c70f551ccbeb464
thzw1142 <thzw1142@163.com>
2020-05-13 00:00:55 +0800

    init and network

--- main.py
+++ main.py
@@ -317,3 +317,260 @@ mask = tf.reduce_sum(detector_mask, axis=2)
 ax2.matshow(mask[..., 0])  # [16, 16]
 
 # %%
+# %%
+from tensorflow.keras import layers
+
+
+import  tensorflow.keras.backend as K 
+
+class SpaceToDepth(layers.Layer):
+
+    def __init__(self, block_size, **kwargs):
+        self.block_size = block_size
+        super(SpaceToDepth, self).__init__(**kwargs)
+
+    def call(self, inputs):
+        x = inputs
+        batch, height, width, depth = K.int_shape(x)
+        batch = -1
+        reduced_height = height // self.block_size
+        reduced_width = width // self.block_size
+        y = K.reshape(x, (batch, reduced_height, self.block_size,
+                             reduced_width, self.block_size, depth))
+        z = K.permute_dimensions(y, (0, 1, 3, 2, 4, 5))
+        t = K.reshape(z, (batch, reduced_height, reduced_width, depth * self.block_size **2))
+        return t
+
+    def compute_output_shape(self, input_shape):
+        shape =  (input_shape[0], input_shape[1] // self.block_size, input_shape[2] // self.block_size,
+                  input_shape[3] * self.block_size **2)
+        return tf.TensorShape(shape)
+
+
+# 3.1
+input_image = layers.Input((IMGSZ,IMGSZ, 3), dtype='float32')
+
+# unit1
+x = layers.Conv2D(32, (3,3), strides=(1,1),padding='same', name='conv_1', use_bias=False)(input_image)
+x = layers.BatchNormalization(name='norm_1')(x)
+x = layers.LeakyReLU(alpha=0.1)(x)
+
+x = layers.MaxPooling2D(pool_size=(2,2))(x)
+
+# unit2
+x = layers.Conv2D(64, (3,3), strides=(1,1), padding='same', name='conv_2',use_bias=False)(x)
+x = layers.BatchNormalization(name='norm_2')(x)
+x = layers.LeakyReLU(alpha=0.1)(x)
+x = layers.MaxPooling2D(pool_size=(2,2))(x)
+
+
+# Layer 3
+x = layers.Conv2D(128, (3,3), strides=(1,1), padding='same', name='conv_3', use_bias=False)(x)
+x = layers.BatchNormalization(name='norm_3')(x)
+x = layers.LeakyReLU(alpha=0.1)(x)
+
+# Layer 4
+x = layers.Conv2D(64, (1,1), strides=(1,1), padding='same', name='conv_4', use_bias=False)(x)
+x = layers.BatchNormalization(name='norm_4')(x)
+x = layers.LeakyReLU(alpha=0.1)(x)
+
+# Layer 5
+x = layers.Conv2D(128, (3,3), strides=(1,1), padding='same', name='conv_5', use_bias=False)(x)
+x = layers.BatchNormalization(name='norm_5')(x)
+x = layers.LeakyReLU(alpha=0.1)(x)
+x = layers.MaxPooling2D(pool_size=(2, 2))(x)
+
+# Layer 6
+x = layers.Conv2D(256, (3,3), strides=(1,1), padding='same', name='conv_6', use_bias=False)(x)
+x = layers.BatchNormalization(name='norm_6')(x)
+x = layers.LeakyReLU(alpha=0.1)(x)
+
+# Layer 7
+x = layers.Conv2D(128, (1,1), strides=(1,1), padding='same', name='conv_7', use_bias=False)(x)
+x = layers.BatchNormalization(name='norm_7')(x)
+x = layers.LeakyReLU(alpha=0.1)(x)
+
+# Layer 8
+x = layers.Conv2D(256, (3,3), strides=(1,1), padding='same', name='conv_8', use_bias=False)(x)
+x = layers.BatchNormalization(name='norm_8')(x)
+x = layers.LeakyReLU(alpha=0.1)(x)
+x = layers.MaxPooling2D(pool_size=(2, 2))(x)
+
+# Layer 9
+x = layers.Conv2D(512, (3,3), strides=(1,1), padding='same', name='conv_9', use_bias=False)(x)
+x = layers.BatchNormalization(name='norm_9')(x)
+x = layers.LeakyReLU(alpha=0.1)(x)
+
+# Layer 10
+x = layers.Conv2D(256, (1,1), strides=(1,1), padding='same', name='conv_10', use_bias=False)(x)
+x = layers.BatchNormalization(name='norm_10')(x)
+x = layers.LeakyReLU(alpha=0.1)(x)
+
+# Layer 11
+x = layers.Conv2D(512, (3,3), strides=(1,1), padding='same', name='conv_11', use_bias=False)(x)
+x = layers.BatchNormalization(name='norm_11')(x)
+x = layers.LeakyReLU(alpha=0.1)(x)
+
+# Layer 12
+x = layers.Conv2D(256, (1,1), strides=(1,1), padding='same', name='conv_12', use_bias=False)(x)
+x = layers.BatchNormalization(name='norm_12')(x)
+x = layers.LeakyReLU(alpha=0.1)(x)
+
+# Layer 13
+x = layers.Conv2D(512, (3,3), strides=(1,1), padding='same', name='conv_13', use_bias=False)(x)
+x = layers.BatchNormalization(name='norm_13')(x)
+x = layers.LeakyReLU(alpha=0.1)(x)
+
+# for skip connection
+skip_x = x  # [b,32,32,512]
+
+
+x = layers.MaxPooling2D(pool_size=(2, 2))(x)
+
+# Layer 14
+x = layers.Conv2D(1024, (3,3), strides=(1,1), padding='same', name='conv_14', use_bias=False)(x)
+x = layers.BatchNormalization(name='norm_14')(x)
+x = layers.LeakyReLU(alpha=0.1)(x)
+
+# Layer 15
+x = layers.Conv2D(512, (1,1), strides=(1,1), padding='same', name='conv_15', use_bias=False)(x)
+x = layers.BatchNormalization(name='norm_15')(x)
+x = layers.LeakyReLU(alpha=0.1)(x)
+
+# Layer 16
+x = layers.Conv2D(1024, (3,3), strides=(1,1), padding='same', name='conv_16', use_bias=False)(x)
+x = layers.BatchNormalization(name='norm_16')(x)
+x = layers.LeakyReLU(alpha=0.1)(x)
+
+# Layer 17
+x = layers.Conv2D(512, (1,1), strides=(1,1), padding='same', name='conv_17', use_bias=False)(x)
+x = layers.BatchNormalization(name='norm_17')(x)
+x = layers.LeakyReLU(alpha=0.1)(x)
+
+# Layer 18
+x = layers.Conv2D(1024, (3,3), strides=(1,1), padding='same', name='conv_18', use_bias=False)(x)
+x = layers.BatchNormalization(name='norm_18')(x)
+x = layers.LeakyReLU(alpha=0.1)(x)
+
+# Layer 19
+x = layers.Conv2D(1024, (3,3), strides=(1,1), padding='same', name='conv_19', use_bias=False)(x)
+x = layers.BatchNormalization(name='norm_19')(x)
+x = layers.LeakyReLU(alpha=0.1)(x)
+
+# Layer 20
+x = layers.Conv2D(1024, (3,3), strides=(1,1), padding='same', name='conv_20', use_bias=False)(x)
+x = layers.BatchNormalization(name='norm_20')(x)
+x = layers.LeakyReLU(alpha=0.1)(x)
+
+# Layer 21
+skip_x = layers.Conv2D(64, (1,1), strides=(1,1), padding='same', name='conv_21', use_bias=False)(skip_x)
+skip_x = layers.BatchNormalization(name='norm_21')(skip_x)
+skip_x = layers.LeakyReLU(alpha=0.1)(skip_x)
+
+skip_x = SpaceToDepth(block_size=2)(skip_x)
+ 
+ # concat
+ # [b,16,16,1024], [b,16,16,256],=> [b,16,16,1280]
+x = tf.concat([skip_x, x], axis=-1)
+
+# Layer 22
+x = layers.Conv2D(1024, (3,3), strides=(1,1), padding='same', name='conv_22', use_bias=False)(x)
+x = layers.BatchNormalization(name='norm_22')(x)
+x = layers.LeakyReLU(alpha=0.1)(x)
+x = layers.Dropout(0.5)(x) # add dropout
+# [b,16,16,5,7] => [b,16,16,35]
+
+x = layers.Conv2D(5*7, (1,1), strides=(1,1), padding='same', name='conv_23')(x)
+
+output = layers.Reshape((GRIDSZ,GRIDSZ,5,7))(x)
+# create model
+model = keras.models.Model(input_image, output)
+x = tf.random.normal((4,512,512,3))
+out = model(x)
+print('out:', out.shape)
+
+
+
+# %%
+# 3.2
+class WeightReader:
+    def __init__(self, weight_file):
+        self.offset = 4
+        self.all_weights = np.fromfile(weight_file, dtype='float32')
+
+    def read_bytes(self, size):
+        self.offset = self.offset + size
+        return self.all_weights[self.offset - size:self.offset]
+
+    def reset(self):
+        self.offset = 4
+weight_reader = WeightReader('yolo.weights')
+
+
+weight_reader.reset()
+nb_conv = 23
+
+for i in range(1, nb_conv + 1):
+    conv_layer = model.get_layer('conv_' + str(i))
+    conv_layer.trainable = True
+
+    if i < nb_conv:
+        norm_layer = model.get_layer('norm_' + str(i))
+        norm_layer.trainable = True
+
+        size = np.prod(norm_layer.get_weights()[0].shape)
+
+        beta = weight_reader.read_bytes(size)
+        gamma = weight_reader.read_bytes(size)
+        mean = weight_reader.read_bytes(size)
+        var = weight_reader.read_bytes(size)
+
+        weights = norm_layer.set_weights([gamma, beta, mean, var])
+
+    if len(conv_layer.get_weights()) > 1:
+        bias = weight_reader.read_bytes(np.prod(conv_layer.get_weights()[1].shape))
+        kernel = weight_reader.read_bytes(np.prod(conv_layer.get_weights()[0].shape))
+        kernel = kernel.reshape(list(reversed(conv_layer.get_weights()[0].shape)))
+        kernel = kernel.transpose([2, 3, 1, 0])
+        conv_layer.set_weights([kernel, bias])
+    else:
+        kernel = weight_reader.read_bytes(np.prod(conv_layer.get_weights()[0].shape))
+        kernel = kernel.reshape(list(reversed(conv_layer.get_weights()[0].shape)))
+        kernel = kernel.transpose([2, 3, 1, 0])
+        conv_layer.set_weights([kernel])
+
+
+layer = model.layers[-2]  # last convolutional layer
+# print(layer.name)
+layer.trainable = True
+
+weights = layer.get_weights()
+
+new_kernel = np.random.normal(size=weights[0].shape) / (GRIDSZ * GRIDSZ)
+new_bias = np.random.normal(size=weights[1].shape) / (GRIDSZ * GRIDSZ)
+
+layer.set_weights([new_kernel, new_bias])
+
+#%%
+
+model.load_weights('C:\\Users\\86188\\Documents\\study\\tensorflow_xinjiapo\\weights\\ckpt.h5')
+
+#%%
+img, detector_mask, matching_gt_boxes, matching_classes_oh, gt_boxes_grid = next(train_gen)
+
+img, detector_mask, matching_gt_boxes, matching_classes_oh, gt_boxes_grid = \
+    img[0], detector_mask[0], matching_gt_boxes[0], matching_classes_oh[0], gt_boxes_grid[0]
+
+# [b,512,512,3]=>[b,16,16,5,7]=>[16,16,5,x-y-w-h-conf-l1-l2]
+y_pred = model(tf.expand_dims(img, axis=0))[0][...,4]
+# [16,16,5] => [16,16]
+y_pred = tf.reduce_sum(y_pred,axis=2)
+
+fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2)
+ax1.imshow(img)
+# [16,16,5,1]=>[16,16]
+ax2.matshow(tf.reduce_sum(detector_mask,axis=2)[...,0])
+ax3.matshow(y_pred)
+
+
+# %%

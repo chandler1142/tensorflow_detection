7e374f16147cf9a7193eeed5b37bedb61898cb7f
Chandler <chandlerzhao1142@gmail.com.cn>
2020-10-03 21:38:18 +0800

    dt

---
+++ tensorflow2/machine_learning/decision_tree/test1.py
@@ -0,0 +1,42 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn import datasets
+
+iris = datasets.load_iris()
+X = iris.data[:, 2:]
+y = iris.target
+
+print(type(X))
+
+print(X[y == 0, 0])
+
+plt.scatter(X[y == 0, 0], X[y == 0, 1])
+plt.scatter(X[y == 1, 0], X[y == 1, 1])
+plt.scatter(X[y == 2, 0], X[y == 2, 1])
+# plt.show()
+
+from sklearn.tree import DecisionTreeClassifier
+
+dt_clf = DecisionTreeClassifier(max_depth=2, criterion="entropy")
+dt_clf.fit(X, y)
+
+from matplotlib.colors import ListedColormap
+
+
+def plot_decision_boundary(model, axis):
+    x0, x1 = np.meshgrid(
+        np.linspace(axis[0], axis[1], int((axis[1] - axis[0]) * 100)).reshape(-1, 1),
+        np.linspace(axis[2], axis[3], int((axis[3] - axis[2]) * 100)).reshape(-1, 1),
+    )
+    X_new = np.c_[x0.ravel(), x1.ravel()]
+    y_predict = model.predict(X_new)
+    zz = y_predict.reshape(x0.shape)
+    custom_cmap = ListedColormap(['#EF9A9A', '#FFF59D', '#90CAF9'])
+    plt.contourf(x0, x1, zz, cmap=custom_cmap)
+
+
+plot_decision_boundary(dt_clf, axis=[0.5, 7.5, 0, 3])
+plt.scatter(X[y == 0, 0], X[y == 0, 1])
+plt.scatter(X[y == 1, 0], X[y == 1, 1])
+plt.scatter(X[y == 2, 0], X[y == 2, 1])
+# plt.show()
